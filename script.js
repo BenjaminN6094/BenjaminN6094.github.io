eb532dfff30aacbc4e932b9fc05a9e52e53db825
Benjamin <43858612+BenjaminN6094@users.noreply.github.com>
2019-10-07 10:29:33 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let ownerName = "Your Name"
-let userName = "YourUsername";
+let ownerName = "BenjaminN"
+let userName = "BenjaminN6094";
 
 document.querySelectorAll(".owner-name").forEach((e) => {
     e.innerHTML = ownerName;
